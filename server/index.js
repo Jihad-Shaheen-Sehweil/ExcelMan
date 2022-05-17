1260c99c931700279533ecd1763fd47fa2d9243b
jihad Shaheen Sehiel <jshaheen@ram1-edu.qou.edu>
2022-05-17 12:32:57 +0300

    First commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// routes app.use("route string", imported from routes function)
+
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
+
+mongoose
+    .connect(CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+    )
+    .catch((error) => console.log(error.message));
